16553d1187ccfda428acec70007c8e1c71e11c97
Basu <basus@C02PM01VG3QN.local>
2016-08-16 13:57:14 -0700

    updates to postchargesresponse 1

--- src/main/java/com/agilysys/pms/account/model/PostChargesResponse.java
+++ src/main/java/com/agilysys/pms/account/model/PostChargesResponse.java
@@ -1,8 +1,14 @@
 package com.agilysys.pms.account.model;
 
 import java.util.List;
+import java.math.BigDecimal;
 
 public class PostChargesResponse {
+
+    private String groupId;
+    private BigDecimal totalCharge;
+    private BigDecimal chargesCount;
+
     private List<LineItemView> lineItemsList;
 
     public List<LineItemView> getLineItemsList() {
@@ -11,5 +17,29 @@ public class PostChargesResponse {
 
     public void setLineItemsList(List<LineItemView> lineItemsList) {
         this.lineItemsList = lineItemsList;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public BigDecimal getTotalCharge() {
+        return totalCharge;
+    }
+
+    public void setTotalCharge(BigDecimal totalCharge) {
+        this.totalCharge = totalCharge;
+    }
+
+    public BigDecimal getChargesCount() {
+        return chargesCount;
+    }
+
+    public void setChargesCount(BigDecimal chargesCount) {
+        this.chargesCount = chargesCount;
     }
 }
