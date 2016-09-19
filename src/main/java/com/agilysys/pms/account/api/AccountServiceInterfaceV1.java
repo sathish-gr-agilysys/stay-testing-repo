ab0f0f6c582d5dbdcf6a500a21aafd9cf732c24f
Basu <basus@C02PM01VG3QN.local>
2016-09-19 15:55:57 -0700

    VCTRS-51182-new endpoint for viewfolio

--- src/main/java/com/agilysys/pms/account/api/AccountServiceInterfaceV1.java
+++ src/main/java/com/agilysys/pms/account/api/AccountServiceInterfaceV1.java
@@ -55,6 +55,7 @@ import com.agilysys.pms.account.model.LedgerBalancesInfo;
 import com.agilysys.pms.account.model.LineItemAdjustment;
 import com.agilysys.pms.account.model.LineItemTransfer;
 import com.agilysys.pms.account.model.LineItemView;
+import com.agilysys.pms.account.model.FolioViewLineItem;
 import com.agilysys.pms.account.model.NextAccountNumberInfo;
 import com.agilysys.pms.account.model.NonInvoicedARDetail;
 import com.agilysys.pms.account.model.Payment;
@@ -68,6 +69,7 @@ import com.agilysys.pms.account.model.PostingRuleDetailView;
 import com.agilysys.pms.account.model.TaxExemptSettingsByDate;
 import com.agilysys.pms.account.model.UpdateInvoiceLineItemsRequest;
 import com.agilysys.pms.account.model.UpdateInvoiceTermsRequest;
+import com.agilysys.pms.account.model.ViewfoliosRequest;
 import com.agilysys.pms.account.model.tmp.fixup.LedgerBalanceFixup;
 import com.agilysys.pms.common.api.annotation.CreatedOnSuccess;
 import com.agilysys.pms.common.api.annotation.OkOnEmpty;
@@ -296,6 +298,14 @@ public interface AccountServiceInterfaceV1 {
     @PreAuthorize("hasPermission('Required', 'ReadAccounts')")
     List<FolioDetail> getFolios(@PathParam(TENANT_ID) String tenantId, @PathParam(PROPERTY_ID) String propertyId,
           @PathParam(ACCOUNT_ID) String accountId, @QueryParam("") GetFoliosOptionalParameters optionalParameters)
+          throws ServiceException;
+
+    @POST
+    @Path(ACCOUNT_ID_PATH + FOLIO_PATH + FOLIO_ID_PATH)
+    @Produces(MediaType.APPLICATION_JSON)
+    @PreAuthorize("hasPermission('Required', 'ReadAccounts')")
+    List<LineItemView> viewFolio(@PathParam(TENANT_ID) String tenantId, @PathParam(PROPERTY_ID) String propertyId,
+          @PathParam(ACCOUNT_ID) String accountId, @PathParam(FOLIO_ID) String folioId, @QueryParam("detailed") boolean detailed, ViewfoliosRequest viewfoliosRequest)
           throws ServiceException;
 
     /**
