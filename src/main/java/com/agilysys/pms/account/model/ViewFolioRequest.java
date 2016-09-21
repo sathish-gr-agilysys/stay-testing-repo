73cb27d78bb802a563822b3f89a34caaafc8fb01
Basu <basus@hansenth.ad.local>
2016-09-21 13:28:28 -0700

    changing array properties to sets for consistency

--- src/main/java/com/agilysys/pms/account/model/ViewFolioRequest.java
+++ src/main/java/com/agilysys/pms/account/model/ViewFolioRequest.java
@@ -3,29 +3,16 @@ package com.agilysys.pms.account.model;
 /**
  * (C) 2013 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
  */
+
+import java.util.Set;
+
 public class ViewFolioRequest {
 
-    private String[] transactionIds;
-    private String[] parentIds;
+    private Set<String> transactionIds;
+    private Set<String> parentIds;
     private int page;
     private int count;
     private boolean includeDetails;
-
-    public String[] getTransactionIds() {
-        return transactionIds;
-    }
-
-    public void setTransactionIds(String[] transactionIds) {
-        this.transactionIds = transactionIds;
-    }
-
-    public String[] getParentIds() {
-        return parentIds;
-    }
-
-    public void setParentIds(String[] parentIds) {
-        this.parentIds = parentIds;
-    }
 
     public int getPage() {
         return page;
@@ -33,6 +20,22 @@ public class ViewFolioRequest {
 
     public void setPage(int page) {
         this.page = page;
+    }
+
+    public Set<String> getTransactionIds() {
+        return transactionIds;
+    }
+
+    public void setTransactionIds(Set<String> transactionIds) {
+        this.transactionIds = transactionIds;
+    }
+
+    public Set<String> getParentIds() {
+        return parentIds;
+    }
+
+    public void setParentIds(Set<String> parentIds) {
+        this.parentIds = parentIds;
     }
 
     public int getCount() {
