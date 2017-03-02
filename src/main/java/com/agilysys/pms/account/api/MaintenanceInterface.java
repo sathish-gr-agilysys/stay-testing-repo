8325f2e287a8c55f3e92215f7f2958910023093b
Peloquin <peloquina@PELOQUINA2.local>
2017-03-02 12:15:00 -0800

    VCTRS-54568: added repair API for account balances per property and date

--- src/main/java/com/agilysys/pms/account/api/MaintenanceInterface.java
+++ src/main/java/com/agilysys/pms/account/api/MaintenanceInterface.java
@@ -13,19 +13,23 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.agilysys.pms.maintenance.domain.BulkReindexJobDetail;
-import com.agilysys.pms.maintenance.domain.BulkReindexJobResult;
-import com.agilysys.pms.maintenance.domain.BulkReindexRequest;
+import org.joda.time.LocalDate;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.agilysys.platform.common.exception.ServiceException;
 import com.agilysys.platform.common.rguest.exception.RGuestException;
+import com.agilysys.pms.account.model.AccountBalanceRepairResult;
 import com.agilysys.pms.maintenance.model.Job;
 
 @Path("/maintenance")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public interface MaintenanceInterface {
+    String TENANT_ID = "tenantId";
+    String PROPERTY_ID = "propertyId";
+    String REQUEST_ID = "requestId";
+    String DATE = "date";
+
     String ACCOUNT_BALANCES_COLLECTION = "/accountBalances";
     String ES_INDEX_ACCOUNTS = "/elasticsearch/indices";
 
@@ -37,11 +41,19 @@ public interface MaintenanceInterface {
     @POST
     @Path(ACCOUNT_BALANCES_COLLECTION + "/refresh")
     @PreAuthorize("hasPermission('Required', 'Platform_writeTenants')")
-    Job refreshAccountBalances() throws RGuestException, ServiceException;
+    Job repairAccountBalances() throws RGuestException, ServiceException;
+
+    @POST
+    @Path("/tenants/{" + TENANT_ID + "}/properties/{" + PROPERTY_ID + "}" + ACCOUNT_BALANCES_COLLECTION + "/{" + DATE +
+          "}/repair")
+    @PreAuthorize("hasPermission('Required', 'WriteAccounts')")
+    AccountBalanceRepairResult repairAccountBalances(@PathParam(TENANT_ID) String tenantId,
+          @PathParam(PROPERTY_ID) String propertyId, @PathParam(DATE) LocalDate date)
+          throws RGuestException, ServiceException;
 
     @GET
-    @Path("/requests/{requestId}")
-    List<Job> retrieveJobs(@PathParam("requestId") String requestId) throws RGuestException, ServiceException;
+    @Path("/requests/{" + REQUEST_ID + "}")
+    List<Job> retrieveJobs(@PathParam(REQUEST_ID) String requestId) throws RGuestException, ServiceException;
 
     @POST
     @PreAuthorize("hasPermission('Required', 'WriteTenants')")
