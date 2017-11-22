b704d929a3c445f9d46ce2062661d188ab54332d
Thirupathy <Ram.Thirupathy@agilysys.com>
2017-11-22 12:27:19 +0530

    Inventory Item changes

---
+++ src/main/java/com/agilysys/pms/account/model/InventoryItem.java
@@ -0,0 +1,32 @@
+/**
+ * (C) 2017 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
+ */
+package com.agilysys.pms.account.model;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class InventoryItem extends TransactionItem {
+    @JsonProperty(required = true)
+    private int availableCount;
+
+    public int getAvailableCount() {
+        return availableCount;
+    }
+
+    public void setAvailableCount(int availableCount) {
+        this.availableCount = availableCount;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj, Boolean.FALSE);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this, Boolean.FALSE);
+    }
+}
