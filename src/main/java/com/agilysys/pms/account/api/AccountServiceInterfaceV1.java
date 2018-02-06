6c2a2064d6042305baf0088d018f2d5940c84be9
Ip Sam <ip.sam@agilysys.com>
2018-02-06 13:48:56 -0800

    VCTRS-59447 Fixed cod review feedback

--- src/main/java/com/agilysys/pms/account/api/AccountServiceInterfaceV1.java
+++ src/main/java/com/agilysys/pms/account/api/AccountServiceInterfaceV1.java
@@ -690,7 +690,7 @@ public interface AccountServiceInterfaceV1 {
      * @return a LineItemView for Display purposes
      */
     @GET
-    @Path(TASK_ID_PATH)
+    @Path(ACCOUNT_ID_PATH + TASK_ID_PATH)
     @PreAuthorize("hasPermission('Required', 'WriteAccounts')")
     List<LineItemView> getPaymentResult(@PathParam(TENANT_ID) String tenantId, @PathParam(PROPERTY_ID) String propertyId,
            @PathParam(TASK_ID) String taskId) throws Throwable;
