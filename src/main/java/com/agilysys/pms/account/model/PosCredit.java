6395574a21fbd31a23f20b2fff13565949645ab6
Donk, Cale <Cale.Donk@agilysys.com>
2018-03-13 14:20:09 -0700

    VCTRS-58546: WIP

---
+++ src/main/java/com/agilysys/pms/account/model/PosCredit.java
@@ -0,0 +1,8 @@
+/**
+ * (C) 2018 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
+ */
+package com.agilysys.pms.account.model;
+
+public class PosCredit extends Credit {
+
+}
