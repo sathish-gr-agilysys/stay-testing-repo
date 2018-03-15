44c2e73d226a8e41d8385cfdca740c7bbdfed608
Donk, Cale <Cale.Donk@agilysys.com>
2018-03-14 21:46:43 -0700

    VCTRS-58546: WIP.

--- src/main/java/com/agilysys/pms/account/model/PosTransaction.java
+++ src/main/java/com/agilysys/pms/account/model/PosTransaction.java
@@ -3,7 +3,47 @@
  */
 package com.agilysys.pms.account.model;
 
+import java.math.BigDecimal;
+
+import org.joda.time.LocalDate;
+
 public interface PosTransaction {
+    String getAccountId();
+    void setAccountId(String accountId);
+
+    BigDecimal getAmount();
+    void setAmount(BigDecimal amount);
+
+    String getFolioId();
+    void setFolioId(String folioId);
+
+    Boolean getIgnoreRules();
+    void setIgnoreRules(Boolean ignoreRules);
+
+    String getItemId();
+    void setItemId(String itemId);
+
+    String getParentId();
+    void setParentId(String parentId);
+
+    LocalDate getPostingDate();
+    void setPostingDate(LocalDate postingDate);
+
+    int getQuantity();
+    void setQuantity(int quantity);
+
+    String getReason();
+    void setReason(String reason);
+
     String getReceiptTextImage();
     void setReceiptTextImage(String receiptTextImage);
+
+    String getReference();
+    void setReference(String reference);
+
+    String getSourceId();
+    void setSourceId(String sourceId);
+
+    String getTerminalId();
+    void setTerminalId(String terminalId);
 }
