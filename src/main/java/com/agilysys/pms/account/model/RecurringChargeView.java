18e97e3324a4f316076a3b26b4618fe873943f99
PakkirisamyS <sattanathan.pakkirisamy@agilysys.com>
2018-03-27 12:46:31 +0530

    [VCTRS-60138] : soft delete changes for deleteRecurringCharge endpoint

--- src/main/java/com/agilysys/pms/account/model/RecurringChargeView.java
+++ src/main/java/com/agilysys/pms/account/model/RecurringChargeView.java
@@ -30,6 +30,7 @@ public class RecurringChargeView {
     private String overrideReason;
     private String ratePlanName;
     private String ratePlanId;
+    private char isDeleted;
 
     // For a package, this will be the estimated tax for the room charge
     private ChargeTaxAmountInfo estimatedTaxInfo;
@@ -198,5 +199,13 @@ public class RecurringChargeView {
 
     public void setRatePlanId(String ratePlanId) {
         this.ratePlanId = ratePlanId;
+    }
+
+    public char getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(char isDeleted) {
+        this.isDeleted = isDeleted;
     }
 }
