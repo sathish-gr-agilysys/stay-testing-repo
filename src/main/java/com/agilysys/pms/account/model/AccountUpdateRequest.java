d19799e5e0f0f43f4433060cfeb44e22ebaef232
Perumal Mani <perumal.mani@agilysys.com>
2018-06-05 05:32:07 -0400

    VCTRS-61009 Added new model AccountUpdateRequest

---
+++ src/main/java/com/agilysys/pms/account/model/AccountUpdateRequest.java
@@ -0,0 +1,47 @@
+/**
+ * (C) 2018 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
+ */
+
+package com.agilysys.pms.account.model;
+
+import java.util.Set;
+
+public class AccountUpdateRequest {
+
+    private final String type;
+    private String companyProfileId;
+    private String requestId;
+    private Set<String> propertyIds;
+
+    public AccountUpdateRequest(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getCompanyProfileId() {
+        return companyProfileId;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public void setCompanyProfileId(String companyProfileId) {
+        this.companyProfileId = companyProfileId;
+    }
+
+    public Set<String> getPropertyIds() {
+        return propertyIds;
+    }
+
+    public void setPropertyIds(Set<String> propertyIds) {
+        this.propertyIds = propertyIds;
+    }
+}
