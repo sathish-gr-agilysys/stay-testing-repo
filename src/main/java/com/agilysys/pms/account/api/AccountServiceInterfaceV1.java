e3b7c232b7f37e5e6d09ae8ae311e589ccc6121b
Kumaran Sowrirajan <kumaran.sowrirajan@agilysys.com>
2018-06-14 16:53:28 -0400

    Updated contract for job status

--- src/main/java/com/agilysys/pms/account/api/AccountServiceInterfaceV1.java
+++ src/main/java/com/agilysys/pms/account/api/AccountServiceInterfaceV1.java
@@ -1394,8 +1394,7 @@ public interface AccountServiceInterfaceV1 {
     @PreAuthorize("hasPermission('Required', 'ReadAccounts')")
     AccountUpdateResponse findAccountReceivableJobStatus(@PathParam(TENANT_ID) String tenantId,
           @PathParam(PROPERTY_ID) String propertyId,
-          @PathParam(ACCOUNT_ID) String companyProfileId,
-          @QueryParam(REQUEST_TYPE) String requestType)
+          @PathParam(COMPANY_PROFILE_ID) String companyProfileId)
           throws RGuestException, ServiceException;
 
     @GET
