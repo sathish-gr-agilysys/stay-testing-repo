efa3c1ac62181c51da1f7b28e867ed09933089bb
Perumal Mani <perumal.mani@agilysys.com>
2018-06-29 11:57:48 -0400

    removed empty line

--- src/main/java/com/agilysys/pms/account/model/AccountUpdateRequest.java
+++ src/main/java/com/agilysys/pms/account/model/AccountUpdateRequest.java
@@ -1,7 +1,6 @@
 /**
  * (C) 2018 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
  */
-
 package com.agilysys.pms.account.model;
 
 import java.util.Collection;
