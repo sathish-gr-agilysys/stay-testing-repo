6cff95fdce713dcf690be424b7f7e4d808f5159a
Perumal Mani <perumal.mani@agilysys.com>
2018-08-13 14:03:48 -0400

    VCTRS-62909 VCTRS-62635 Added model method and Tenant Level accountservice interface

---
+++ src/main/java/com/agilysys/pms/account/api/AccountServiceTenantInterface.java
@@ -0,0 +1,36 @@
+/**
+ * (C) 2018 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
+ */
+package com.agilysys.pms.account.api;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import com.agilysys.platform.common.exception.ServiceException;
+import com.agilysys.platform.common.rguest.exception.RGuestException;
+import com.agilysys.pms.account.model.ARBalanceInfo;
+
+@Path(AccountServiceTenantInterface.BASE_PATH)
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public interface AccountServiceTenantInterface {
+    String TENANT_ID = "tenantId";
+
+    String PROPERTY_DATE = "propertyDate";
+    String BASE_PATH = "/tenants/{" + TENANT_ID + "}/accounts";
+
+    String AR_BALANCES_PATH = "/ar/balances";
+
+    @GET
+    @Path(AR_BALANCES_PATH)
+    @PreAuthorize("hasPermission('Required', 'ReadAccountsReceivable')")
+    ARBalanceInfo getARAccountBalance(@PathParam(TENANT_ID) String tenantId,
+          @QueryParam("type") String type) throws RGuestException, ServiceException;
+}
---
+++ src/main/java/com/agilysys/pms/account/model/ARBalanceInfo.java
@@ -0,0 +1,188 @@
+/**
+ * (C) 2018 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
+ */
+package com.agilysys.pms.account.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class ARBalanceInfo {
+    private String tenantId;
+    private List<String> ageGroupLabels;
+    private List<CompanyBalanceInfo> companyProfiles;
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public List<String> getAgeGroupLabels() {
+        return ageGroupLabels;
+    }
+
+    public void setAgeGroupLabels(List<String> ageGroupLabels) {
+        this.ageGroupLabels = ageGroupLabels;
+    }
+
+    public List<CompanyBalanceInfo> getCompanyProfiles() {
+        return companyProfiles;
+    }
+
+    public void setCompanyProfiles(List<CompanyBalanceInfo> companyProfiles) {
+        this.companyProfiles = companyProfiles;
+    }
+
+    public static class CompanyBalanceInfo {
+        private String id;
+        private String name;
+        private String deliveryPreference;
+
+        private List<BigDecimal> invoiceTotalsByAging; //invoiceTotalByAgeGroup
+        private BigDecimal unInvoicedTotal; // sum(unInvoicedCharges)
+
+        private List<PropertyBalanceInfo> properties;
+
+        public CompanyBalanceInfo(String id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDeliveryPreference() {
+            return deliveryPreference;
+        }
+
+        public void setDeliveryPreference(String deliveryPreference) {
+            this.deliveryPreference = deliveryPreference;
+        }
+
+        public List<BigDecimal> getInvoiceTotalsByAging() { return invoiceTotalsByAging; }
+
+        public void setInvoiceTotalsByAging(List<BigDecimal> invoiceTotalsByAging) {
+            this.invoiceTotalsByAging = invoiceTotalsByAging;
+        }
+
+        public BigDecimal getInvoiceTotal() { // invoiceTotal = sum(invoiceTotalsByAgeGroups)
+            if (invoiceTotalsByAging == null ) return BigDecimal.ZERO;
+            return invoiceTotalsByAging.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+        }
+
+        public BigDecimal getUnInvoicedTotal() {
+            return unInvoicedTotal;
+        }
+
+        public void setUnInvoicedTotal(BigDecimal unInvoicedTotal) {
+            this.unInvoicedTotal = unInvoicedTotal;
+        }
+
+        public BigDecimal getBalance() {
+            return getInvoiceTotal().add(getUnInvoicedTotal()); // balance = subTotal + unInvoicedTotal
+        }
+
+        public List<PropertyBalanceInfo> getProperties() {
+            return properties;
+        }
+
+        public void setProperties(List<PropertyBalanceInfo> properties) {
+            this.properties = properties;
+        }
+
+        public static class PropertyBalanceInfo {
+            private String id;
+            private String name;
+            private String deliveryPreference;
+
+            private boolean readOnly;
+            private boolean closed;
+
+            private List<BigDecimal> invoiceTotalsByAging; //invoiceTotalByAgeGroup
+            private BigDecimal unInvoicedTotal; // sum(unInvoicedCharges)
+
+            public PropertyBalanceInfo(String id, String name) {
+                this.id = id;
+                this.name = name;
+            }
+
+            public String getId() {
+                return id;
+            }
+
+            public void setId(String id) {
+                this.id = id;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public String getDeliveryPreference() {
+                return deliveryPreference;
+            }
+
+            public void setDeliveryPreference(String deliveryPreference) {
+                this.deliveryPreference = deliveryPreference;
+            }
+
+            public boolean isReadOnly() {
+                return readOnly;
+            }
+
+            public void setReadOnly(boolean readOnly) {
+                this.readOnly = readOnly;
+            }
+
+            public boolean isClosed() {
+                return closed;
+            }
+
+            public void setClosed(boolean closed) {
+                this.closed = closed;
+            }
+
+            public List<BigDecimal> getInvoiceTotalsByAging() { return invoiceTotalsByAging; }
+
+            public void setInvoiceTotalsByAging(List<BigDecimal> invoiceTotalsByAging) {
+                this.invoiceTotalsByAging = invoiceTotalsByAging;
+            }
+
+            public BigDecimal getInvoiceTotal() { // invoiceTotal = sum(invoiceTotalsByAgeGroups)
+                if (invoiceTotalsByAging == null ) return BigDecimal.ZERO;
+                return invoiceTotalsByAging.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+            }
+
+            public BigDecimal getUnInvoicedTotal() {
+                return unInvoicedTotal == null? BigDecimal.ZERO: unInvoicedTotal;
+            }
+
+            public void setUnInvoicedTotal(BigDecimal unInvoicedTotal) {
+                this.unInvoicedTotal = unInvoicedTotal;
+            }
+
+            public BigDecimal getBalance() {
+                return getInvoiceTotal().add(getUnInvoicedTotal()); // balance = subTotal + unInvoicedTotal
+            }
+        }
+    }
+}
