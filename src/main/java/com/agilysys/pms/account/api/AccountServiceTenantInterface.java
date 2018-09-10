cac4be5d09b59e0b402dfb5a18b3adb6755cad46
Kumaran Sowrirajan <kumaran.sowrirajan@agilysys.com>
2018-09-10 09:58:52 -0400

    Added new API to recalculate the balances

--- src/main/java/com/agilysys/pms/account/api/AccountServiceTenantInterface.java
+++ src/main/java/com/agilysys/pms/account/api/AccountServiceTenantInterface.java
@@ -3,9 +3,13 @@
  */
 package com.agilysys.pms.account.api;
 
+import java.util.List;
+import java.util.Set;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,6 +21,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import com.agilysys.platform.common.exception.ServiceException;
 import com.agilysys.platform.common.rguest.exception.RGuestException;
 import com.agilysys.pms.account.model.ARBalanceInfo;
+import com.agilysys.pms.account.model.InvoiceBalanceResponse;
 
 @Path(AccountServiceTenantInterface.BASE_PATH)
 @Produces(MediaType.APPLICATION_JSON)
@@ -28,6 +33,8 @@ public interface AccountServiceTenantInterface {
     String BASE_PATH = "/tenants/{" + TENANT_ID + "}/accounts";
 
     String AR_BALANCES_PATH = "/ar/balances";
+    String CALCULATE_INVOICE_BALANCE = "/calculateBalance";
+    String INVOICES_PATH = "/invoices";
 
     @GET
     @Path(AR_BALANCES_PATH)
@@ -36,4 +43,12 @@ public interface AccountServiceTenantInterface {
           "or hasPermission('Required', 'ReadAccountsReceivable')")
     ARBalanceInfo getARAccountBalance(@PathParam(TENANT_ID) String tenantId,
           @DefaultValue("AGE_BY_PROPERTY_DATE") @QueryParam("type") String type) throws RGuestException, ServiceException;
+
+    @PUT
+    @Path(INVOICES_PATH + CALCULATE_INVOICE_BALANCE)
+    @PreAuthorize("hasPermission('Required', 'WriteAccountsReceivable')" +
+          " or hasPermission('Required', 'UseAccountsReceivable')")
+    List<InvoiceBalanceResponse> updateInvoiceBalanceByInvoiceNumber(@PathParam(TENANT_ID) String tenantId,
+          Set<String> invoiceNumbers)
+          throws RGuestException, ServiceException;
 }
