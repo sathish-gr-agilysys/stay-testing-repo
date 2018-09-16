042cc09e40e82c35b1a4e67f42cfb83715c432a9
Kumaran Sowrirajan <kumaran.sowrirajan@agilysys.com>
2018-09-16 19:55:08 -0400

    moved the nested class to a separate class based on code review

---
+++ src/main/java/com/agilysys/pms/account/model/CompanyBalanceInfo.java
@@ -0,0 +1,92 @@
+package com.agilysys.pms.account.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.agilysys.pms.account.model.AccountAttributes.PreferredCommunication;
+
+public class CompanyBalanceInfo {
+
+    private String id;
+    private String name;
+    private PreferredCommunication deliveryPreference;
+    private String email;
+
+    private List<BigDecimal> invoiceTotalsByAging; //invoiceTotalByAgeGroup
+    private BigDecimal invoicedTotal; //  sum(invoiceTotalsByAgeGroups)
+    private BigDecimal unInvoicedTotal;
+    private BigDecimal balance; // subTotal + unInvoicedTotal
+
+    private List<PropertyBalanceInfo> properties;
+
+    public CompanyBalanceInfo(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PreferredCommunication getDeliveryPreference() {
+        return deliveryPreference;
+    }
+
+    public void setDeliveryPreference(PreferredCommunication deliveryPreference) {
+        this.deliveryPreference = deliveryPreference;
+    }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public List<BigDecimal> getInvoiceTotalsByAging() { return invoiceTotalsByAging; }
+
+    public void setInvoiceTotalsByAging(List<BigDecimal> invoiceTotalsByAging) {
+        this.invoiceTotalsByAging = invoiceTotalsByAging;
+    }
+
+    public BigDecimal getInvoiceTotal() {
+        return invoicedTotal;
+    }
+
+    public void setInvoiceTotal(BigDecimal invoicedTotal) {
+        this.invoicedTotal = invoicedTotal;
+    }
+
+    public BigDecimal getUnInvoicedTotal() {
+        return unInvoicedTotal;
+    }
+
+    public void setUnInvoicedTotal(BigDecimal unInvoicedTotal) {
+        this.unInvoicedTotal = unInvoicedTotal;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public List<PropertyBalanceInfo> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(List<PropertyBalanceInfo> properties) {
+        this.properties = properties;
+    }
+}
