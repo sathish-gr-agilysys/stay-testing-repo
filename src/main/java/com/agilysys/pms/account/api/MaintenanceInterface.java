f2c229855a28bd69ebc0f62c1173a41f0b640fa8
Donk, Cale <Cale.Donk@agilysys.com>
2018-11-14 22:42:36 -0800

    VCTRS-62760: Maintenance fixes.

--- src/main/java/com/agilysys/pms/account/api/MaintenanceInterface.java
+++ src/main/java/com/agilysys/pms/account/api/MaintenanceInterface.java
@@ -1,4 +1,4 @@
-/**
+/*
  * (C) 2016 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
  */
 package com.agilysys.pms.account.api;
@@ -20,9 +20,16 @@ import com.agilysys.platform.common.exception.ServiceException;
 import com.agilysys.platform.common.rguest.exception.RGuestException;
 import com.agilysys.pms.maintenance.model.IndexRequest;
 
-@Path("/maintenance/elasticsearch")
+@Path(MaintenanceInterface.BASE_PATH)
 @Consumes(MediaType.APPLICATION_JSON)
 public interface MaintenanceInterface {
+    String BASE_PATH = "/maintenance";
+
+    String ACCOUNTS_PATH = "/accounts";
+    String INDEX_PATH = "/index";
+    String RANGE_PATH = "/range";
+    String UNINDEXED_PATH = "/unindexed";
+
     String TENANT_ID = "tenantId";
     String TENANT_ID_TEMPLATE = "{" + TENANT_ID + "}";
 
@@ -35,21 +42,26 @@ public interface MaintenanceInterface {
     String WRITE_TENANTS_PERMISSION = PERMISSION_PREFIX + "WriteTenants" + PERMISSION_POSTFIX;
 
     @GET
-    @Path("/unindexed/accounts/" + TENANT_ID_TEMPLATE)
+    @Path(RANGE_PATH + ACCOUNTS_PATH + "/" + TENANT_ID_TEMPLATE)
+    long countRangeAccounts(@PathParam(TENANT_ID) String tenantId, @QueryParam(UPDATED_SINCE) DateTime updatedSince,
+          @QueryParam(UPDATED_UNTIL) DateTime updatedUntil) throws RGuestException, ServiceException;
+
+    @GET
+    @Path(UNINDEXED_PATH + ACCOUNTS_PATH + "/" + TENANT_ID_TEMPLATE)
     long countUnindexedAccounts(@PathParam(TENANT_ID) String tenantId) throws RGuestException, ServiceException;
 
     @GET
-    @Path("/unindexed/accounts")
+    @Path(UNINDEXED_PATH + ACCOUNTS_PATH)
     Map<String, Long> countUnindexedAccounts() throws RGuestException, ServiceException;
 
     @POST
     @PreAuthorize(WRITE_TENANTS_PERMISSION)
-    @Path("/index/accounts/" + TENANT_ID_TEMPLATE)
+    @Path(INDEX_PATH + ACCOUNTS_PATH + "/" + TENANT_ID_TEMPLATE)
     int indexAccounts(@PathParam(TENANT_ID) String tenantId, @QueryParam(UPDATED_SINCE) DateTime updatedSince,
           @QueryParam(UPDATED_UNTIL) DateTime updatedUntil) throws RGuestException, ServiceException;
 
     @POST
     @PreAuthorize(WRITE_TENANTS_PERMISSION)
-    @Path("/index/accounts")
+    @Path(INDEX_PATH + ACCOUNTS_PATH)
     Map<String, Integer> indexAccounts(IndexRequest request) throws RGuestException, ServiceException;
 }
