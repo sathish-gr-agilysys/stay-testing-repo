487ba3e06c98d40d8d2a0f753d57891110522374
visalam <visalam@KrishnanV.ad.local>
2018-12-07 18:48:59 +0530

    VCTRS-65022

--- src/main/java/com/agilysys/pms/account/api/ReportingServiceInterface.java
+++ src/main/java/com/agilysys/pms/account/api/ReportingServiceInterface.java
@@ -157,7 +157,7 @@ public interface ReportingServiceInterface {
     RevenueReportResult getRevenueDetailReport(@PathParam(TENANT_ID) String tenantId,
           @PathParam(PROPERTY_ID) String propertyId, @QueryParam(START_DATE) LocalDate startDate,
           @QueryParam(END_DATE) LocalDate endDate, @QueryParam(ROOM_REVENUE) Boolean isRoomRevenue,
-          @QueryParam(REV_OCC) Boolean revOcc) throws RGuestException, ServiceException;
+          @QueryParam(REV_OCC) boolean revOcc) throws RGuestException, ServiceException;
 
     /**
      * retrieve recurring charge detail information for a given date
