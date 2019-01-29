773eed721100cb2d5e00ee4a8b0854719986394a
Ganga Priya Muthaluraj <ganga@MUTHALURAJG.ad.local>
2019-01-29 11:28:46 +0530

    VCTRS-64216:test case

--- src/main/java/com/agilysys/pms/account/model/Transaction.java
+++ src/main/java/com/agilysys/pms/account/model/Transaction.java
@@ -11,6 +11,8 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.joda.time.LocalDate;
 
 import com.agilysys.common.model.rate.CompInfo;
+import com.agilysys.common.model.rate.ComponentType;
+import com.agilysys.common.model.rate.RoomChargePostingType;
 import com.agilysys.platform.common.json.schema.MaxLengthRestriction;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -28,6 +30,8 @@ public abstract class Transaction {
     protected BigDecimal amount;
     protected String callType;
     protected CompInfo compInfo;
+    protected RoomChargePostingType roomChargePostingType;
+    protected ComponentType componentType;
     protected String folioId;
     protected BigDecimal freeAllowanceAmount = BigDecimal.ZERO;
     protected Boolean ignoreRules = true;
@@ -70,6 +74,22 @@ public abstract class Transaction {
 
     public void setCompInfo(CompInfo compInfo) {
         this.compInfo = compInfo;
+    }
+
+    public RoomChargePostingType getRoomChargePostingType() {
+        return roomChargePostingType;
+    }
+
+    public void setRoomChargePostingType(RoomChargePostingType roomChargePostingType) {
+        this.roomChargePostingType = roomChargePostingType;
+    }
+
+    public ComponentType getComponentType() {
+        return componentType;
+    }
+
+    public void setComponentType(ComponentType componentType) {
+        this.componentType = componentType;
     }
 
     public String getFolioId() {
