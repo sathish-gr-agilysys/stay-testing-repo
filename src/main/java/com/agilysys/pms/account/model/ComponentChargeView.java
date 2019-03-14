ef4e2c3da4a493974489a9f33f2771f79925bb07
sattanathan <pakkirisamys@PakkirisamyS.ad.local>
2019-03-14 15:31:18 +0530

    VCTRS-64203: Fixing estimated charges loading for flat tax of package issue

--- src/main/java/com/agilysys/pms/account/model/ComponentChargeView.java
+++ src/main/java/com/agilysys/pms/account/model/ComponentChargeView.java
@@ -177,8 +177,11 @@ public class ComponentChargeView {
     public static List<ComponentChargeView> fromComponentRateSnapshots(
           List<ComponentRateSnapshot> componentRateSnapshots) {
         List<ComponentChargeView> componentChargeViews = new ArrayList<>(componentRateSnapshots.size());
-        componentRateSnapshots.stream().forEach(
-              componentRateSnapshot -> componentChargeViews.add(fromComponentRateSnapshot(componentRateSnapshot)));
+        componentRateSnapshots.stream().forEach(componentRateSnapshot -> {
+            if (componentRateSnapshot.getTotalQuantity() != null && componentRateSnapshot.getTotalQuantity() > 0) {
+                componentChargeViews.add(fromComponentRateSnapshot(componentRateSnapshot));
+            }
+        });
         return componentChargeViews;
     }
 }
