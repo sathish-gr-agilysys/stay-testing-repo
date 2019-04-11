ab64c0669dbe2673fe0c5e387923b9d4fd50181b
Donk, Cale <Cale.Donk@agilysys.com>
2019-04-10 17:43:23 -0700

    VCTRS-62762: Merged in changes.

--- src/main/java/com/agilysys/pms/account/model/AccountStatement.java
+++ src/main/java/com/agilysys/pms/account/model/AccountStatement.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * (C) 2017 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
  */
@@ -43,41 +42,3 @@ public class AccountStatement {
         return Optional.ofNullable(folioBalances.get(folioId)).map(Balance::getTotal).orElse(BigDecimal.ZERO);
     }
 }
-=======
-/**
- * (C) 2017 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
- */
-package com.agilysys.pms.account.model;
-
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
-public class AccountStatement {
-
-    private Balance balance = new Balance();
-
-    private Map<String, Balance> folioBalances = new HashMap<>();
-
-    public Balance getBalance() {
-        return balance;
-    }
-
-    public void setBalance(Balance balance) {
-        this.balance = balance;
-    }
-
-    public Map<String, Balance> getFolioBalances() {
-        return folioBalances;
-    }
-
-    public void setFolioBalances(Map<String, Balance> folioBalances) {
-        this.folioBalances = folioBalances;
-    }
-
-    public BigDecimal getFolioTotal(String folioId) {
-        return Optional.ofNullable(folioBalances.get(folioId)).map(Balance::getTotal).orElse(BigDecimal.ZERO);
-    }
-}
->>>>>>> 54b6432e3e3187b895886d46553c7a7126bc701d
