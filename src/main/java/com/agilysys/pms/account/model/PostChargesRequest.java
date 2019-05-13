5f4675e562d84adbc85d9f9e3f47f03277e5d906
Thirupathy <Ram.Thirupathy@agilysys.com>
2019-05-13 17:35:05 +0530

    VCTRS-65400: Fixig Grouped charges bug

--- src/main/java/com/agilysys/pms/account/model/PostChargesRequest.java
+++ src/main/java/com/agilysys/pms/account/model/PostChargesRequest.java
@@ -9,6 +9,7 @@ public class PostChargesRequest {
 
     private List<Charge> charges;
     private boolean distributed;
+    private boolean grouped;
     private boolean isRecurring;
     private String name;
 
@@ -26,6 +27,14 @@ public class PostChargesRequest {
 
     public void setDistributed(boolean distributed) {
         this.distributed = distributed;
+    }
+
+    public boolean isGrouped() {
+        return grouped;
+    }
+
+    public void setGrouped(boolean grouped) {
+        this.grouped = grouped;
     }
 
     public String getName() {
