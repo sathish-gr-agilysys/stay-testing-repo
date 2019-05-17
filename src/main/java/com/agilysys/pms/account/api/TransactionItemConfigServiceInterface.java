213d848b8aa9c93328c826548d3fd4d1675e56f3
SenthilkumarMJ <SenthilkumarMJ@MugunthanS.ad.local>
2019-05-17 12:11:48 +0530

    include comp permit query param added in the api

--- src/main/java/com/agilysys/pms/account/api/TransactionItemConfigServiceInterface.java
+++ src/main/java/com/agilysys/pms/account/api/TransactionItemConfigServiceInterface.java
@@ -41,6 +41,7 @@ public interface TransactionItemConfigServiceInterface {
     String INCLUDE_INTERNAL = "includeInternal";
     String INCLUDE_SUB_TRANSACTION_ITEMS = "includeSubTransactionItems";
     String INCLUDE_INACTIVE = "includeInactive";
+    String INCLUDE_PETMIT_COMP = "includePermitComp";
     String COMTROL_VALUE = "comtrolValue";
     String COMTROL_VALUE_PATH = COMTROL_VALUE + "/{comtrolValue}";
     String ACTIVE = "/active";
@@ -151,7 +152,7 @@ public interface TransactionItemConfigServiceInterface {
     List<TransactionItem> getActiveTransactionItem(@PathParam(TENANT_ID) String tenantId,
           @PathParam(PROPERTY_ID) String propertyId, @QueryParam(INCLUDE_INTERNAL) boolean includeInternal,
           @QueryParam(INCLUDE_SUB_TRANSACTION_ITEMS) boolean includeSubItems,
-          @QueryParam(INCLUDE_INACTIVE) boolean includeInactive) throws RGuestException, ServiceException;
+          @QueryParam(INCLUDE_INACTIVE) boolean includeInactive, @QueryParam(INCLUDE_PETMIT_COMP) boolean includePermitComp) throws RGuestException, ServiceException;
 
     /**
      * Endpoint to update transaction to new collection, this end point
