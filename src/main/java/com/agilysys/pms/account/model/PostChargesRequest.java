89ab34021a830bc0596d27a48785c1012a15076a
Palanisamy <Sabarivasan.Palanisamy@agilysys.com>
2019-05-20 17:50:44 +0530

    VCTRS-66992 posting date added to PostingRequest

--- src/main/java/com/agilysys/pms/account/model/PostChargesRequest.java
+++ src/main/java/com/agilysys/pms/account/model/PostChargesRequest.java
@@ -5,12 +5,15 @@ package com.agilysys.pms.account.model;
 
 import java.util.List;
 
+import org.joda.time.LocalDate;
+
 public class PostChargesRequest {
 
     private List<Charge> charges;
     private boolean distributed;
     private boolean isRecurring;
     private String name;
+    private LocalDate postingDate;
     private boolean postRecurringCharge;
 
 
@@ -43,6 +46,14 @@ public class PostChargesRequest {
 
     public void setIsRecurring(boolean isRecurring) {
         this.isRecurring = isRecurring;
+    }
+
+    public LocalDate getPostingDate() {
+        return postingDate;
+    }
+
+    public void setPostingDate(LocalDate postingDate) {
+        this.postingDate = postingDate;
     }
 
     public boolean isPostRecurringCharge() {
