db022f826eaec9a83f5d9bbcbf59f6ab61748d57
sattanathan <pakkirisamys@PakkirisamyS.ad.local>
2019-06-20 11:42:27 +0530

    VCTRS-65404: Removing unwanted lines

--- src/main/java/com/agilysys/pms/account/model/FolioInvoiceLineItemView.java
+++ src/main/java/com/agilysys/pms/account/model/FolioInvoiceLineItemView.java
@@ -19,17 +19,7 @@ public class FolioInvoiceLineItemView extends FolioViewLineItem {
     private String creditMessage;
     private String balanceTransferMessage;
     private String transferInMessage;
-    private String formattedGrandTotalAmount;
-    private String formattedLineItemBalance;
-    private String formattedLineItemTaxBalance;
-    private String formattedLineItemChargeBalance;
     private String formattedUnitAmount;
-    private String formattedAdjustmentsGrandTotalAmount;
-    private String formattedAdjustmentsTaxAmount;
-    private String formattedCorrectionGrandTotalAmount;
-    private String formattedRefundGrandTotalAmount;
-    private String formattedTaxAmount;
-    private String formattedCorrectionTaxAmount;
     private boolean showReason;
     private List<FolioInvoiceLineItemView> adjustmentFolioLineItems;
     private List<FolioInvoiceLineItemView> groupedFolioTaxLineItems;
@@ -134,92 +124,12 @@ public class FolioInvoiceLineItemView extends FolioViewLineItem {
         this.transferInMessage = transferInMessage;
     }
 
-    public String getFormattedGrandTotalAmount() {
-        return formattedGrandTotalAmount;
-    }
-
-    public void setFormattedGrandTotalAmount(String formattedGrandTotalAmount) {
-        this.formattedGrandTotalAmount = formattedGrandTotalAmount;
-    }
-
-    public String getFormattedLineItemBalance() {
-        return formattedLineItemBalance;
-    }
-
-    public void setFormattedLineItemBalance(String formattedLineItemBalance) {
-        this.formattedLineItemBalance = formattedLineItemBalance;
-    }
-
-    public String getFormattedLineItemTaxBalance() {
-        return formattedLineItemTaxBalance;
-    }
-
-    public void setFormattedLineItemTaxBalance(String formattedLineItemTaxBalance) {
-        this.formattedLineItemTaxBalance = formattedLineItemTaxBalance;
-    }
-
-    public String getFormattedLineItemChargeBalance() {
-        return formattedLineItemChargeBalance;
-    }
-
-    public void setFormattedLineItemChargeBalance(String formattedLineItemChargeBalance) {
-        this.formattedLineItemChargeBalance = formattedLineItemChargeBalance;
-    }
-
     public String getFormattedUnitAmount() {
         return formattedUnitAmount;
     }
 
     public void setFormattedUnitAmount(String formattedUnitAmount) {
         this.formattedUnitAmount = formattedUnitAmount;
-    }
-
-    public String getFormattedAdjustmentsGrandTotalAmount() {
-        return formattedAdjustmentsGrandTotalAmount;
-    }
-
-    public void setFormattedAdjustmentsGrandTotalAmount(String formattedAdjustmentsGrandTotalAmount) {
-        this.formattedAdjustmentsGrandTotalAmount = formattedAdjustmentsGrandTotalAmount;
-    }
-
-    public String getFormattedAdjustmentsTaxAmount() {
-        return formattedAdjustmentsTaxAmount;
-    }
-
-    public void setFormattedAdjustmentsTaxAmount(String formattedAdjustmentsTaxAmount) {
-        this.formattedAdjustmentsTaxAmount = formattedAdjustmentsTaxAmount;
-    }
-
-    public String getFormattedCorrectionGrandTotalAmount() {
-        return formattedCorrectionGrandTotalAmount;
-    }
-
-    public void setFormattedCorrectionGrandTotalAmount(String formattedCorrectionGrandTotalAmount) {
-        this.formattedCorrectionGrandTotalAmount = formattedCorrectionGrandTotalAmount;
-    }
-
-    public String getFormattedRefundGrandTotalAmount() {
-        return formattedRefundGrandTotalAmount;
-    }
-
-    public void setFormattedRefundGrandTotalAmount(String formattedRefundGrandTotalAmount) {
-        this.formattedRefundGrandTotalAmount = formattedRefundGrandTotalAmount;
-    }
-
-    public String getFormattedTaxAmount() {
-        return formattedTaxAmount;
-    }
-
-    public void setFormattedTaxAmount(String formattedTaxAmount) {
-        this.formattedTaxAmount = formattedTaxAmount;
-    }
-
-    public String getFormattedCorrectionTaxAmount() {
-        return formattedCorrectionTaxAmount;
-    }
-
-    public void setFormattedCorrectionTaxAmount(String formattedCorrectionTaxAmount) {
-        this.formattedCorrectionTaxAmount = formattedCorrectionTaxAmount;
     }
 
     public List<FolioInvoiceLineItemView> getAdjustmentFolioLineItems() {
@@ -276,73 +186,5 @@ public class FolioInvoiceLineItemView extends FolioViewLineItem {
 
     public void setShowReason(boolean showReason) {
         this.showReason = showReason;
-    }
-
-    public FolioInvoiceLineItemView lineItemToFolioInvoiceViewLineItem(LineItemView lineItemView) {
-        setTaxAmount(lineItemView.getTaxAmount());
-        setTotalAmount(lineItemView.getTotalAmount());
-        setGrandTotalAmount(lineItemView.getGrandTotalAmount());
-        setLineItemChargesBalance(lineItemView.getLineItemChargesBalance());
-        setLineItemBalance(lineItemView.getLineItemBalance());
-        setLineItemTaxBalance(lineItemView.getLineItemTaxBalance());
-        setQuantity(lineItemView.getQuantity());
-        setAdjustmentsGrandTotalAmount(lineItemView.getAdjustmentsTotalAmount());
-        setAdjustmentsTaxAmount(lineItemView.getAdjustmentsTaxAmount());
-        setAdjustmentsTotalAmount(lineItemView.getAdjustmentsTotalAmount());
-        setCorrectionGrandTotalAmount(lineItemView.getCorrectionGrandTotalAmount());
-        setCorrectionTaxAmount(lineItemView.getCorrectionTaxAmount());
-        setCorrectionTotalAmount(lineItemView.getCorrectionTotalAmount());
-        setRefundGrantTotalAmount(lineItemView.getRefundGrantTotalAmount());
-        setTransferGrandTotalAmount(lineItemView.getTransferGrandTotalAmount());
-        setTransferTaxAmount(lineItemView.getTransferTaxAmount());
-        setTransferTotalAmount(lineItemView.getTransferTotalAmount());
-        setAccountId(lineItemView.getAccountId());
-        setAutoRecurringItemId(lineItemView.getAutoRecurringItemId());
-        setAutoRouted(lineItemView.isAutoRouted());
-        setCallType(lineItemView.getCallType());
-        setCategoryId(lineItemView.getCategoryId());
-        setCompInfo(lineItemView.getCompInfo());
-        setComponentType(lineItemView.getComponentType());
-        setDescription(lineItemView.getDescription());
-        setDisplayDate(lineItemView.getDisplayDate());
-        setDistributed(lineItemView.isDistributed());
-        setFolioId(lineItemView.getFolioId());
-        setFolioLineType(lineItemView.getFolioLineType());
-        setFreeAllowanceAmount(lineItemView.getFreeAllowanceAmount());
-        setId(lineItemView.getId());
-        setInvalid(lineItemView.isInvalid());
-        setItemId(lineItemView.getItemId());
-        setLedgerId(lineItemView.getLedgerId());
-        setLedgerTransactionHistory(lineItemView.getLedgerTransactionHistory());
-        setLedgerTransactionHistoryId(lineItemView.getLedgerTransactionHistoryId());
-        setMealPeriodId(lineItemView.getMealPeriodId());
-        setName(lineItemView.getName());
-        setParentId(lineItemView.getParentId());
-        setPetDisplayName(lineItemView.getPetDisplayName());
-        setPostingCalendarDateTime(lineItemView.getPostingCalendarDateTime());
-        setPostingPropertyDate(lineItemView.getPostingPropertyDate());
-        setRateChangeComment(lineItemView.getRateChangeComment());
-        setReason(lineItemView.getReason());
-        setReceiptTextImage(lineItemView.getReceiptTextImage());
-        setRecurringChargeId(lineItemView.getRecurringChargeId());
-        setReference(lineItemView.getReference());
-        setReservationId(lineItemView.getReservationId());
-        setReverseTax(lineItemView.isReverseTax());
-        setReverseTaxTotalChargeAmount(lineItemView.getReverseTaxTotalChargeAmount());
-        setRoomChargePostingType(lineItemView.getRoomChargePostingType());
-        setRoomRevenue(lineItemView.isRoomRevenue());
-        setRouted(lineItemView.isRouted());
-        setSourceId(lineItemView.getSourceId());
-        setSubcategoryId(lineItemView.getSubcategoryId());
-        setTransactionDestinationAccountId(lineItemView.getTransactionDestinationAccountId());
-        setTransactionId(lineItemView.getTransactionId());
-        setTransactionMessageAttributes(lineItemView.getTransactionMessageAttributes());
-        setTransactionSourceAccountId(lineItemView.getTransactionSourceAccountId());
-        setTransactionType(lineItemView.getTransactionType());
-        setUnitAmount(lineItemView.getUnitAmount());
-        setUserId(lineItemView.getUserId());
-        setReverseTaxTotalChargeAmount(lineItemView.getReverseTaxTotalChargeAmount());
-        setReverseTax(lineItemView.isReverseTax());
-        return this;
     }
 }
