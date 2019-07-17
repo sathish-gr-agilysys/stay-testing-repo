728ab947636877f5aefc5b0993b19be93091757f
Parthiban <Nandakumar.Parthiban@agilysys.com>
2019-07-17 19:28:53 +0530

    VCTRS-66455 -WIP

--- src/main/java/com/agilysys/pms/account/model/GiftCardRequest.java
+++ src/main/java/com/agilysys/pms/account/model/GiftCardRequest.java
@@ -3,21 +3,9 @@
  */
 package com.agilysys.pms.account.model;
 
-import com.agilysys.pms.payment.model.GatewayType;
-
 public class GiftCardRequest {
-    private String cardNumber;
     private Charge charge;
     private String invoiceNumber;
-    private GatewayType gatewayType;
-
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
-    }
 
     public Charge getCharge() {
         return charge;
@@ -33,13 +21,5 @@ public class GiftCardRequest {
 
     public void setInvoiceNumber(String invoiceNumber) {
         this.invoiceNumber = invoiceNumber;
-    }
-
-    public GatewayType getGatewayType() {
-        return gatewayType;
-    }
-
-    public void setGatewayType(GatewayType gatewayType) {
-        this.gatewayType = gatewayType;
     }
 }
