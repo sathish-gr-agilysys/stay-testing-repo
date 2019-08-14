68626ddcaa18655a3e6e879cfafca43efc2782b2
Donk, Cale <Cale.Donk@agilysys.com>
2019-08-14 10:47:56 -0700

    VCTRS-67825: WIP.

--- src/main/java/com/agilysys/pms/account/model/RevenueReportResult.java
+++ src/main/java/com/agilysys/pms/account/model/RevenueReportResult.java
@@ -1,3 +1,6 @@
+/*
+ * (C) 2019 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
+ */
 package com.agilysys.pms.account.model;
 
 import java.util.List;
@@ -11,6 +14,12 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 public class RevenueReportResult {
     @JsonDeserialize(keyUsing = LocalDateJsonKeyDeserializer.class)
     private Map<LocalDate, List<RevenueReportItem>> revenueDetails;
+
+    public RevenueReportResult() {}
+
+    public RevenueReportResult(Map<LocalDate, List<RevenueReportItem>> revenueDetails) {
+        this.revenueDetails = revenueDetails;
+    }
 
     public Map<LocalDate, List<RevenueReportItem>> getRevenueDetails() {
         return revenueDetails;
