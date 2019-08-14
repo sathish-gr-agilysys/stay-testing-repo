8f7bf6acef11763f6b1d145b77aabd321070183e
Donk, Cale <Cale.Donk@agilysys.com>
2019-08-14 15:40:16 -0700

    VCTRS-67825: WIP.

--- src/main/java/com/agilysys/pms/account/api/MaintenanceInterface.java
+++ src/main/java/com/agilysys/pms/account/api/MaintenanceInterface.java
@@ -4,6 +4,7 @@
 package com.agilysys.pms.account.api;
 
 import java.util.Map;
+import java.util.Set;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -13,7 +14,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import org.joda.time.DateTime;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.agilysys.platform.common.exception.ServiceException;
@@ -26,12 +26,14 @@ public interface MaintenanceInterface {
     String BASE_PATH = "/maintenance";
 
     String ACCOUNTS_PATH = "/accounts";
-    String LEDGER_TRANSACTIONS_PATH = "/ledgertransactions";
+    String LEDGER_TRANSACTIONS_PATH = "/ledgerTransactions";
+    String REVENUE_AGGREGATES_PATH = "/revenueAggregates";
 
     String COUNT_PATH = "/count";
     String EXPORT_PATH = "/export";
     String INDEX_PATH = "/index";
     String RANGE_PATH = "/range";
+    String SEED_EXPORT_PATH = "/seedExport";
     String UNINDEXED_PATH = "/unindexed";
     String UNEXPORTED_PATH = "/unexported";
 
@@ -114,4 +116,40 @@ public interface MaintenanceInterface {
     @PreAuthorize(WRITE_TENANTS_PERMISSION)
     @Path(EXPORT_PATH + LEDGER_TRANSACTIONS_PATH)
     Map<String, Long> exportLedgerTransactions(IndexRequest request) throws RGuestException, ServiceException;
+
+    @GET
+    @Path(COUNT_PATH + RANGE_PATH + REVENUE_AGGREGATES_PATH + "/" + TENANT_ID_TEMPLATE)
+    long countRangeRevenueAggregates(@PathParam(TENANT_ID) String tenantId,
+          @QueryParam(UPDATED_SINCE) String updatedSince, @QueryParam(UPDATED_UNTIL) String updatedUntil)
+          throws RGuestException, ServiceException;
+
+    @GET
+    @Path(COUNT_PATH + UNEXPORTED_PATH + REVENUE_AGGREGATES_PATH + "/" + TENANT_ID_TEMPLATE)
+    long countUnexportedRevenueAggregates(@PathParam(TENANT_ID) String tenantId)
+          throws RGuestException, ServiceException;
+
+    @GET
+    @Path(COUNT_PATH + UNEXPORTED_PATH + REVENUE_AGGREGATES_PATH)
+    Map<String, Long> countUnexportedRevenueAggregates() throws RGuestException, ServiceException;
+
+    @POST
+    @PreAuthorize(WRITE_TENANTS_PERMISSION)
+    @Path(EXPORT_PATH + REVENUE_AGGREGATES_PATH + "/" + TENANT_ID_TEMPLATE)
+    long exportRevenueAggregates(@PathParam(TENANT_ID) String tenantId, @QueryParam(UPDATED_SINCE) String updatedSince,
+          @QueryParam(UPDATED_UNTIL) String updatedUntil) throws RGuestException, ServiceException;
+
+    @POST
+    @PreAuthorize(WRITE_TENANTS_PERMISSION)
+    @Path(EXPORT_PATH + REVENUE_AGGREGATES_PATH)
+    Map<String, Long> exportRevenueAggregates(IndexRequest request) throws RGuestException, ServiceException;
+
+    @POST
+    @PreAuthorize(WRITE_TENANTS_PERMISSION)
+    @Path(SEED_EXPORT_PATH + REVENUE_AGGREGATES_PATH + "/" + TENANT_ID_TEMPLATE)
+    long seedRevenueAggregates(@PathParam(TENANT_ID) String tenantId) throws RGuestException, ServiceException;
+
+    @POST
+    @PreAuthorize(WRITE_TENANTS_PERMISSION)
+    @Path(SEED_EXPORT_PATH + REVENUE_AGGREGATES_PATH)
+    Map<String, Long> seedRevenueAggregates(Set<String> tenantIds) throws RGuestException, ServiceException;
 }
