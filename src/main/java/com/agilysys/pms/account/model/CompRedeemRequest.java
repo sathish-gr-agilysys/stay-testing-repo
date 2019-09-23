0e0143fc6ee367a2c0807a31bc4e16a9f215a158
pritamp <pritamp@pritamp1.ad.local>
2019-09-23 13:36:40 +0530

    VCTRS-68069 included integration interface in pom.xml and compRedeemRequest changes

--- src/main/java/com/agilysys/pms/account/model/CompRedeemRequest.java
+++ src/main/java/com/agilysys/pms/account/model/CompRedeemRequest.java
@@ -10,4 +10,36 @@ public class CompRedeemRequest {
     private BigDecimal amount;
     private String pin;
     private String playerOrCardId;
+
+    public String getLineItemId() {
+        return lineItemId;
+    }
+
+    public void setLineItemId(String lineItemId) {
+        this.lineItemId = lineItemId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public String getPlayerOrCardId() {
+        return playerOrCardId;
+    }
+
+    public void setPlayerOrCardId(String playerOrCardId) {
+        this.playerOrCardId = playerOrCardId;
+    }
 }
