6fc27f0976e89aece1049bbcdd56bdc5deda3e7a
mohameddawood <MohamedDawood.Washif@agilysys.com>
2019-10-02 13:39:02 +0530

    VCTRS-68069 auth and dept name

--- src/main/java/com/agilysys/pms/account/model/EligibleFolioLineItems.java
+++ src/main/java/com/agilysys/pms/account/model/EligibleFolioLineItems.java
@@ -11,15 +11,19 @@ public class EligibleFolioLineItems {
     private List<LineItemView> lineItemViews;
     private String authorizerId;
     private String departmentId;
+    private String authorizerName;
+    private String departmentName;
 
     public EligibleFolioLineItems() {
         lineItemViews = new ArrayList<>();
     }
 
-    public EligibleFolioLineItems(List<LineItemView> lineItemViews, String authorizerId, String departmentId) {
+    public EligibleFolioLineItems(List<LineItemView> lineItemViews, String authorizerId, String departmentId, String authorizerName, String departmentName) {
         this.lineItemViews = lineItemViews;
         this.authorizerId = authorizerId;
         this.departmentId = departmentId;
+        this.authorizerName = authorizerName;
+        this.departmentName = departmentName;
     }
 
     public List<LineItemView> getLineItemViews() {
@@ -44,5 +48,21 @@ public class EligibleFolioLineItems {
 
     public void setDepartmentId(String departmentId) {
         this.departmentId = departmentId;
+    }
+
+    public String getAuthorizerName() {
+        return authorizerName;
+    }
+
+    public void setAuthorizerName(String authorizerName) {
+        this.authorizerName = authorizerName;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
     }
 }
