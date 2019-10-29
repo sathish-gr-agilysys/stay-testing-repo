ee2be01c3ad4a5a2d96a9a2b2eb99bf38f16667c
Vigneshwar <vignesh@VigneshwarS.ad.local>
2019-10-29 20:17:13 +0530

    VCTRS-6617 : Review Comments addressed

--- src/main/java/com/agilysys/pms/account/model/PostCancellationDetail.java
+++ src/main/java/com/agilysys/pms/account/model/PostCancellationDetail.java
@@ -1,3 +1,6 @@
+/*
+ * (C) 2019 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
+ */
 package com.agilysys.pms.account.model;
 
 public class PostCancellationDetail {
@@ -7,6 +10,19 @@ public class PostCancellationDetail {
     private String errorMessage;
 
     private String errorCode;
+
+    public PostCancellationDetail() {
+    }
+
+    public PostCancellationDetail(boolean success) {
+        this.success = success;
+    }
+
+    public PostCancellationDetail(boolean success, String errorMessage, String errorCode) {
+        this.success = success;
+        this.errorMessage = errorMessage;
+        this.errorCode = errorCode;
+    }
 
     public boolean isSuccess() {
         return success;
