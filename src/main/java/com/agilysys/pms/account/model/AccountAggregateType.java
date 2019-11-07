8bf97aa2a82d4d8c14f08aaa47307db09d7dd4fd
Jacob Ward <jacob.ward@agilysys.com>
2019-11-06 18:13:29 -0800

    VCTRS-67825: PR fixes

--- src/main/java/com/agilysys/pms/account/model/AccountAggregateType.java
+++ src/main/java/com/agilysys/pms/account/model/AccountAggregateType.java
@@ -6,5 +6,14 @@ package com.agilysys.pms.account.model;
 import com.agilysys.pms.common.model.export.AggregateType;
 
 public enum AccountAggregateType implements AggregateType {
-    REVENUE
+    REVENUE;
+
+    // This is implicitly used by Jersey when converting @PathParam/@QueryParam
+    public static AccountAggregateType fromString(String s) {
+        try {
+            return valueOf(s.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw AggregateType.aggregateTypeUnsupported(s);
+        }
+    }
 }
