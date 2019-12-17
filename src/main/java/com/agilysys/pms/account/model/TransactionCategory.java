9f8122c880070371a97e7810e056d432876ce759
Suriyakumar <natarajansu@NatarajansU1.ad.local>
2019-12-17 18:32:24 +0530

    VCTRS-70375 Stay Info out - adding revenue details

--- src/main/java/com/agilysys/pms/account/model/TransactionCategory.java
+++ src/main/java/com/agilysys/pms/account/model/TransactionCategory.java
@@ -38,6 +38,16 @@ public class TransactionCategory extends AccountingObjectBase {
     @AuditField(name = "defaultSource", references = { EntityTypes.BUILDING, EntityTypes.OUTLET })
     private String defaultSourceId;
 
+    @JsonProperty(required = true)
+    @DataPortReference(name = "htngCode", type = {Building.class, Outlet.class})
+    @AuditField(name = "htngCode", references = {EntityTypes.BUILDING, EntityTypes.OUTLET})
+    private Integer htngCode;
+
+    @JsonProperty(required = true)
+    @DataPortReference(name = "htngDescription", type = {Building.class, Outlet.class})
+    @AuditField(name = "htngDescription", references = {EntityTypes.BUILDING, EntityTypes.OUTLET})
+    private String htngDescription;
+
     /**
      * @return the sourceIds
      */
@@ -58,6 +68,22 @@ public class TransactionCategory extends AccountingObjectBase {
 
     public void setDefaultSourceId(String defaultSourceId) {
         this.defaultSourceId = defaultSourceId;
+    }
+
+    public Integer getHtngCode() {
+        return htngCode;
+    }
+
+    public void setHtngCode(Integer htngCode) {
+        this.htngCode = htngCode;
+    }
+
+    public String getHtngDescription() {
+        return htngDescription;
+    }
+
+    public void setHtngDescription(String htngDescription) {
+        this.htngDescription = htngDescription;
     }
 
     /** {@inheritDoc} */
