612f273c56db4285a78297cfbdc7784dd69b157c
Haja Sharif J <haja.jaffaralli@agilysys.com>
2020-03-31 15:02:43 +0530

    VCTRS-69777 changes to fix demo bugs

--- src/main/java/com/agilysys/pms/account/model/AutoRecurringItemResponse.java
+++ src/main/java/com/agilysys/pms/account/model/AutoRecurringItemResponse.java
@@ -6,6 +6,23 @@ public class AutoRecurringItemResponse extends AutoRecurringItem {
     public AutoRecurringItemResponse() {
     }
 
+    public AutoRecurringItemResponse(AutoRecurringItem autoRecurringItem) {
+        this.id = autoRecurringItem.getId();
+        this.value = autoRecurringItem.getValue();
+        this.valueType = autoRecurringItem.getValueType();
+        this.basedOn = autoRecurringItem.getBasedOn();
+        this.defaultSourceId = autoRecurringItem.getDefaultSourceId();
+        this.frequencyType = autoRecurringItem.getFrequencyType();
+        this.occurrenceDays = autoRecurringItem.getOccurrenceDays();
+        this.parentTransactionItemId = autoRecurringItem.getParentTransactionItemId();
+        this.status = autoRecurringItem.getStatus();
+        this.nNights = autoRecurringItem.getnNights();
+        this.startDate = autoRecurringItem.getStartDate();
+        this.endDate = autoRecurringItem.getEndDate();
+        this.description = autoRecurringItem.getDescription();
+        this.applicableFor = autoRecurringItem.getApplicableFor();
+    }
+
     public int getQuantity() {
         return quantity;
     }
