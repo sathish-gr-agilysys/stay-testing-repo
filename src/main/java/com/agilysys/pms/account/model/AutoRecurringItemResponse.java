1ff50cdbc5525d03e762d0fa6933b8461b086df6
yuvaraj <velusamyy@VelusamyYu.ad.local>
2020-04-05 16:55:22 +0530

    VCTRS:69777 added Copyright

--- src/main/java/com/agilysys/pms/account/model/AutoRecurringItemResponse.java
+++ src/main/java/com/agilysys/pms/account/model/AutoRecurringItemResponse.java
@@ -1,3 +1,6 @@
+/*
+ * (C) 2020 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
+ */
 package com.agilysys.pms.account.model;
 
 public class AutoRecurringItemResponse extends AutoRecurringItem {
