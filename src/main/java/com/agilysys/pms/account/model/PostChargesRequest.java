087e38a12ee18c49ef2a7823ea87f5a51eb05b7a
Parthiban <Nandakumar.Parthiban@agilysys.com>
2020-04-09 02:21:00 +0530

    VCTRS-71837 : addOn posting date

--- src/main/java/com/agilysys/pms/account/model/PostChargesRequest.java
+++ src/main/java/com/agilysys/pms/account/model/PostChargesRequest.java
@@ -16,7 +16,7 @@ public class PostChargesRequest {
     private String name;
     private LocalDate postingDate;
     private boolean postRecurringCharge;
-
+    private String componentBundleId;
 
     public List<Charge> getCharges() {
         return charges;
@@ -71,6 +71,14 @@ public class PostChargesRequest {
 
     public void setPostRecurringCharge(boolean postRecurringCharge) {
         this.postRecurringCharge = postRecurringCharge;
+    }
+
+    public String getComponentBundleId() {
+        return componentBundleId;
+    }
+
+    public void setComponentBundleId(String componentBundleId) {
+        this.componentBundleId = componentBundleId;
     }
 }
 
