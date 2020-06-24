09d964fa286a4ad041b76145d9d3de7291c8974e
mohameddawood <MohamedDawood.Washif@agilysys.com>
2020-06-24 20:22:15 +0530

    VCTRS-73145 threshold client

---
+++ src/main/java/com/agilysys/pms/account/client/ThresholdAmountServiceClient.java
@@ -0,0 +1,16 @@
+package com.agilysys.pms.account.client;
+
+import com.agilysys.pms.account.api.ThresholdAmountInterface;
+import com.agilysys.pms.common.auth.ApiKeyAuthHandler;
+import com.agilysys.pms.common.client.StayServiceClient;
+
+public class ThresholdAmountServiceClient extends StayServiceClient<ThresholdAmountInterface> {
+
+    public ThresholdAmountServiceClient(String uri) {
+        super(ThresholdAmountInterface.class, uri);
+    }
+
+    public ThresholdAmountServiceClient(String uri, ApiKeyAuthHandler apiKeyAuthHandler) {
+        super(ThresholdAmountInterface.class, uri, apiKeyAuthHandler);
+    }
+}
