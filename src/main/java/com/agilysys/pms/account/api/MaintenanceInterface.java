f7f875ee9a28e8789067c2d0bf4e3880efa4daee
Donk, Cale <Cale.Donk@agilysys.com>
2020-07-23 10:08:59 -0700

    VCTRS-75556: Added search health check endpoint stubs.

--- src/main/java/com/agilysys/pms/account/api/MaintenanceInterface.java
+++ src/main/java/com/agilysys/pms/account/api/MaintenanceInterface.java
@@ -5,44 +5,23 @@ package com.agilysys.pms.account.api;
 
 import java.util.Map;
 
-import javax.ws.rs.Consumes;
-
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.agilysys.platform.common.rguest.exception.RGuestException;
+import com.agilysys.pms.common.api.search.SearchMaintenanceInterface;
 import com.agilysys.pms.maintenance.model.MaintenanceRequest;
 
-@Path(MaintenanceInterface.BASE_PATH)
-@Consumes(MediaType.APPLICATION_JSON)
-public interface MaintenanceInterface {
-    String BASE_PATH = "/maintenance";
-
+public interface MaintenanceInterface extends SearchMaintenanceInterface {
     String ACCOUNTS_PATH = "/accounts";
-    String COUNT_PATH = "/count";
-    String INDEX_PATH = "/index";
-    String RANGE_PATH = "/range";
-    String UNINDEXED_PATH = "/unindexed";
-
-    String TENANT_ID = "tenantId";
-    String TENANT_ID_TEMPLATE = "{" + TENANT_ID + "}";
-
-    String UPDATED_SINCE = "updatedSince";
-    String UPDATED_UNTIL = "updatedUntil";
-
-    String PERMISSION_PREFIX = "hasPermission('Required', '";
-    String PERMISSION_POSTFIX = "')";
-
-    String WRITE_TENANTS_PERMISSION = PERMISSION_PREFIX + "WriteTenants" + PERMISSION_POSTFIX;
 
     @GET
     @Path(COUNT_PATH + RANGE_PATH + ACCOUNTS_PATH + "/" + TENANT_ID_TEMPLATE)
