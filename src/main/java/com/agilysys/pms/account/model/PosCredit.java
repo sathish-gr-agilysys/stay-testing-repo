d2c9c44bf33d0bb5c68fae012f2ddbbcbdb98b1a
mohameddawood <MohamedDawood.Washif@agilysys.com>
2020-07-31 17:16:11 +0530

    VCTRS-75175 house account charge credit calls

--- src/main/java/com/agilysys/pms/account/model/PosCredit.java
+++ src/main/java/com/agilysys/pms/account/model/PosCredit.java
@@ -8,6 +8,28 @@ public class PosCredit extends Credit implements PosTransaction {
     private Long checkNumber;
     private String closeTime;
 
+    public PosCredit() {}
+
+    public PosCredit(PosCredit posCredit) {
+        this.receiptTextImage = posCredit.getReceiptTextImage();
+        this.checkNumber = posCredit.getCheckNumber();
+        this.closeTime = posCredit.getCloseTime();
+        this.setTransactionItemType(posCredit.getTransactionItemType());
+        this.setMealPeriodId(posCredit.getMealPeriodId());
+        this.setExpectedGrossAmount(posCredit.getExpectedGrossAmount());
+        this.setAccountId(posCredit.getAccountId());
+        this.setAmount(posCredit.getAmount());
+        this.setIgnoreRules(posCredit.getIgnoreRules());
+        this.setItemId(posCredit.getItemId());
+        this.setParentId(posCredit.getParentId());
+        this.setPostingDate(posCredit.getPostingDate());
+        this.setQuantity(posCredit.getQuantity());
+        this.setReason(posCredit.getReason());
+        this.setReference(posCredit.getReference());
+        this.setSourceId(posCredit.getSourceId());
+        this.setTerminalId(posCredit.getTerminalId());
+    }
+
     @Override
     public String getReceiptTextImage() {
         return receiptTextImage;
@@ -35,4 +57,11 @@ public class PosCredit extends Credit implements PosTransaction {
     public void setCloseTime(String closeTime) {
         this.closeTime = closeTime;
     }
+
+    @Override
+    public boolean checkPosCharge() {
+        return true;
+    }
 }
+
+
