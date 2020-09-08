f2b4f3e00ea83c83b30bae76288bac5c52cb1a7c
Sivaprakasam, Sharmila <Sharmila.Sivaprakasam@agilysys.com>
2020-09-08 18:37:34 +0530

    RGSTAY-1210 additinal fields

--- src/main/java/com/agilysys/pms/account/model/EstimatedChargeDetailsForOffers.java
+++ src/main/java/com/agilysys/pms/account/model/EstimatedChargeDetailsForOffers.java
@@ -13,6 +13,18 @@ public class EstimatedChargeDetailsForOffers {
     Map<LocalDate, List<RecurringChargeView>> recurringChargeViewsByDate;
     Map<String, BigDecimal> offerWiseAmountSplit;
     Map<String, BigDecimal> offerWiseTaxSplit;
+    private BigDecimal originalAmount;
+    private BigDecimal originalTaxAmount;
+
+    public EstimatedChargeDetailsForOffers(Map<LocalDate, List<RecurringChargeView>> recurringChargeViewsByDate,
+          Map<String, BigDecimal> offerWiseSplit, Map<String, BigDecimal> offerWiseTaxSplit, BigDecimal originalAmount,
+          BigDecimal originalTaxAmount) {
+        this.recurringChargeViewsByDate = recurringChargeViewsByDate;
+        this.offerWiseAmountSplit = offerWiseSplit;
+        this.offerWiseTaxSplit = offerWiseTaxSplit;
+        this.originalAmount = originalAmount;
+        this.originalTaxAmount = originalTaxAmount;
+    }
 
     public Map<LocalDate, List<RecurringChargeView>> getRecurringChargeViewsByDate() {
         return recurringChargeViewsByDate;
@@ -36,5 +48,21 @@ public class EstimatedChargeDetailsForOffers {
 
     public void setOfferWiseTaxSplit(Map<String, BigDecimal> offerWiseTaxSplit) {
         this.offerWiseTaxSplit = offerWiseTaxSplit;
+    }
+
+    public BigDecimal getOriginalAmount() {
+        return originalAmount;
+    }
+
+    public void setOriginalAmount(BigDecimal originalAmount) {
+        this.originalAmount = originalAmount;
+    }
+
+    public BigDecimal getOriginalTaxAmount() {
+        return originalTaxAmount;
+    }
+
+    public void setOriginalTaxAmount(BigDecimal originalTaxAmount) {
+        this.originalTaxAmount = originalTaxAmount;
     }
 }
--- src/main/java/com/agilysys/pms/account/model/OfferDetails.java
+++ src/main/java/com/agilysys/pms/account/model/OfferDetails.java
@@ -4,13 +4,18 @@
 package com.agilysys.pms.account.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.joda.time.LocalDate;
 
 import com.agilysys.common.model.rate.ComponentRateSnapshot;
 import com.agilysys.common.model.rate.OfferSnapshot;
+import com.agilysys.pms.common.model.RateCalculationBaseType;
+import com.agilysys.pms.common.model.ValueType;
 
 public class OfferDetails {
     private List<OfferSnapshot> offerSnapshots;
@@ -30,6 +35,55 @@ public class OfferDetails {
 
     public void setRateSnapshots(List<RateDetails> rateSnapshots) {
         this.rateSnapshots = rateSnapshots;
+    }
+
+    public static Map<LocalDate, List<RecurringChargeView>> getRecurringChargeFromSnapShot(OfferDetails offerDetails) {
+        Map<LocalDate, List<RecurringChargeView>> recurringChargeViewsByDate = new HashMap<>();
+        for (RateDetails rateDetails : offerDetails.getRateSnapshots()) {
+            List<RecurringChargeView> recurringChargeViews = new ArrayList<>();
+            RecurringChargeView recurringChargeView = new RecurringChargeView();
+            recurringChargeView.setRatePlanId(rateDetails.getRatePlanId());
+            recurringChargeView.setChargeDate(rateDetails.getDate());
+            recurringChargeView.setAmount(rateDetails.getRoomRate());
+            recurringChargeView.setItemId(rateDetails.getTransactionItemId());
+            if (org.apache.commons.collections.CollectionUtils.isNotEmpty(rateDetails.getComponentRates())) {
+                List<ComponentChargeView> componentCharges =
+                      ComponentChargeView.fromComponentRateSnapshots(rateDetails.getComponentRates());
+                recurringChargeView.setComponentCharges(componentCharges);
+            }
+            recurringChargeViews.add(recurringChargeView);
+            if (org.apache.commons.collections.CollectionUtils.isNotEmpty(rateDetails.getAutoRecurringCharges())) {
+                for (AutoRecurringItemResponse autoRecurringItem : rateDetails.getAutoRecurringCharges()) {
+                    RecurringChargeView autoRecurringChargeView = new RecurringChargeView();
+                    autoRecurringChargeView.setChargeDate(rateDetails.getDate());
+                    if (ValueType.PERCENT.equals(autoRecurringItem.getValueType())) {
+                        BigDecimal roomRate = rateDetails.getRoomRate();
+                        if (autoRecurringItem.getBasedOn()
+                              .equals(RateCalculationBaseType.NIGHTLY_ROOM_CHARGE_WITH_COMPONENTS) &&
+                              org.apache.commons.collections.CollectionUtils
+                                    .isNotEmpty(rateDetails.getComponentRates())) {
+                            for (ComponentRateSnapshot componentRateSnapshot : rateDetails.getComponentRates()) {
+                                roomRate = roomRate.add(componentRateSnapshot.getTotalAmount());
+                            }
+                        }
+                        BigDecimal baseAmount =
+                              roomRate.multiply(autoRecurringItem.getValue().divide(new BigDecimal(100)));
+                        autoRecurringChargeView
+                              .setAmount(baseAmount.multiply(new BigDecimal(autoRecurringItem.getQuantity())));
+                    } else {
+                        autoRecurringChargeView.setAmount(
+                              autoRecurringItem.getValue().multiply(new BigDecimal(autoRecurringItem.getQuantity())));
+                    }
+                    autoRecurringChargeView.setQuantity(1);
+                    autoRecurringChargeView.setAutoRecurringItemId(autoRecurringItem.getId());
+                    autoRecurringChargeView.setAutoRecurringRuleId(autoRecurringItem.getAutoRecurringRuleId());
+                    autoRecurringChargeView.setItemId(autoRecurringItem.getParentTransactionItemId());
+                    recurringChargeViews.add(autoRecurringChargeView);
+                }
+            }
+            recurringChargeViewsByDate.put(rateDetails.getDate(), recurringChargeViews);
+        }
+        return recurringChargeViewsByDate;
     }
 
     public static class RateDetails {
