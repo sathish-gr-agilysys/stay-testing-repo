1a64227ffb07a20871b589c6bdfc0c38b3b34f74
mohameddawood <MohamedDawood.Washif@agilysys.com>
2020-11-13 21:42:43 +0530

    RGSTAY-13634 fixing PR comments

---
+++ src/main/java/com/agilysys/pms/account/model/IGLedgertransaction.java
@@ -0,0 +1,14 @@
+package com.agilysys.pms.account.model;
+
+public class IGLedgertransaction {
+
+    private String receiptTextImage;
+
+    public String getReceiptTextImage() {
+        return receiptTextImage;
+    }
+
+    public void setReceiptTextImage(String receiptTextImage) {
+        this.receiptTextImage = receiptTextImage;
+    }
+}
