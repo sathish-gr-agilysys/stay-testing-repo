64473f0e1c13e90e2f2e5ecb360b6dc0c4441553
Aaron Peloquin <aaron.peloquin@agilysys.com>
2021-01-21 14:12:24 -0800

    RGSTAY-20191: completed first pass on inbound rates

--- src/main/java/com/agilysys/pms/account/client/TransactionItemServiceClient.java
+++ src/main/java/com/agilysys/pms/account/client/TransactionItemServiceClient.java
@@ -5,14 +5,19 @@ package com.agilysys.pms.account.client;
 
 import com.agilysys.pms.account.api.TransactionItemConfigServiceInterface;
 import com.agilysys.pms.common.auth.ApiKeyAuthHandler;
-import com.agilysys.pms.common.client.StayServiceClient;
+import com.agilysys.pms.common.client.caching.CachingClient;
+import com.agilysys.pms.common.client.caching.CachingClientContext;
 
-public class TransactionItemServiceClient extends StayServiceClient<TransactionItemConfigServiceInterface> {
+public class TransactionItemServiceClient extends CachingClient<TransactionItemConfigServiceInterface> {
     public TransactionItemServiceClient(String uri) {
-        super(TransactionItemConfigServiceInterface.class, uri);
+        super(TransactionItemConfigServiceInterface.class, uri, null);
     }
 
     public TransactionItemServiceClient(String uri, ApiKeyAuthHandler apiKeyAuthHandler) {
-        super(TransactionItemConfigServiceInterface.class, uri, apiKeyAuthHandler);
+        super(TransactionItemConfigServiceInterface.class, uri, apiKeyAuthHandler, null);
+    }
+
+    public TransactionItemServiceClient(String uri, ApiKeyAuthHandler apiKeyAuthHandler, CachingClientContext context) {
+        super(TransactionItemConfigServiceInterface.class, uri, apiKeyAuthHandler, context);
     }
 }
