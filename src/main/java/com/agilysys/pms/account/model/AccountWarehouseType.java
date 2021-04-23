29737a41618c5cbb512310a37568c2fcf349f5b1
mohameddawood <MohamedDawood.Washif@agilysys.com>
2021-04-23 20:20:53 +0530

    RGSTAY-29722 queue entity changes

--- src/main/java/com/agilysys/pms/account/model/AccountWarehouseType.java
+++ src/main/java/com/agilysys/pms/account/model/AccountWarehouseType.java
@@ -17,7 +17,7 @@ public enum AccountWarehouseType implements WarehouseType {
     SUBCATEGORIES,
     TRANSACTION_ITEMS,
     PANTRY_ITEMS,
-    INVENTORY_ITEM_ALLOCATION;
+    INVENTORY_ITEM_ALLOCATIONS;
 
     // This is implicitly used by Jersey when converting @PathParam/@QueryParam
     public static AccountWarehouseType fromString(String s) {
