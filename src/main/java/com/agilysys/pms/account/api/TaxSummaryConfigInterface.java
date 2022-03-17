981deec4d6c5c8c25cdcaa2d990399fa952290d4
Kabilan <Kabilan.Manivannan@agilysys.com>
2022-03-17 15:47:16 +0530

    RGSTAY-139019:VAT breakdown setup

--- src/main/java/com/agilysys/pms/account/api/TaxSummaryConfigInterface.java
+++ src/main/java/com/agilysys/pms/account/api/TaxSummaryConfigInterface.java
@@ -2,9 +2,11 @@ package com.agilysys.pms.account.api;
 
 import com.agilysys.platform.common.rguest.exception.RGuestException;
 import com.agilysys.pms.account.model.TaxSummaryConfig;
+import com.agilysys.pms.account.model.TaxSummaryConfigSettings;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+
 import java.util.List;
 import java.util.Map;
 
@@ -12,24 +14,45 @@ import java.util.Map;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public interface TaxSummaryConfigInterface {
-    String ID ="id";
+    String ID = "id";
     String TENANT_ID = "tenantId";
     String PROPERTY_ID = "propertyId";
     String TENANT_TEMPLATE = "{" + TENANT_ID + "}";
     String PROPERTY_TEMPLATE = "{" + PROPERTY_ID + "}";
     String BASE = "/tenants/" + TENANT_TEMPLATE + "/properties/" + PROPERTY_TEMPLATE;
+    String TAX_SUMMARY_CONFIG_PATH = "/setup/accounts/taxSummaryConfigSettings";
+    String TAX_SUMMARY_CONFIG = "/taxSummaryConfigSettings";
 
     @GET
     @Path("/taxSummaryConfig")
-    List<TaxSummaryConfig> getTaxSummaryConfig(@PathParam(TENANT_ID) String tenantId, @PathParam(PROPERTY_ID) String propertyId) throws RGuestException;
+    List<TaxSummaryConfig> getTaxSummaryConfig(@PathParam(TENANT_ID) String tenantId,
+          @PathParam(PROPERTY_ID) String propertyId) throws RGuestException;
 
     @POST
     @Path("/taxSummaryConfig")
-    void postTaxSummaryConfig(@PathParam(TENANT_ID) String tenantId, @PathParam(PROPERTY_ID) String propertyId, TaxSummaryConfig taxSummaryConfig) throws RGuestException;
+    void postTaxSummaryConfig(@PathParam(TENANT_ID) String tenantId, @PathParam(PROPERTY_ID) String propertyId,
+          TaxSummaryConfig taxSummaryConfig) throws RGuestException;
 
     @PUT
     @Path("/taxSummaryConfig/{id}")
-    TaxSummaryConfig updateTaxSummaryConfig(@PathParam(TENANT_ID) String tenantId, @PathParam(PROPERTY_ID) String propertyId, @PathParam(ID) String id, TaxSummaryConfig taxSummaryConfig) throws RGuestException;
+    TaxSummaryConfig updateTaxSummaryConfig(@PathParam(TENANT_ID) String tenantId,
+          @PathParam(PROPERTY_ID) String propertyId, @PathParam(ID) String id, TaxSummaryConfig taxSummaryConfig)
+          throws RGuestException;
 
+    @GET
+    @Path(TAX_SUMMARY_CONFIG)
+    TaxSummaryConfigSettings getTaxSummaryConfigSettings(@PathParam(TENANT_ID) String tenantId,
+          @PathParam(PROPERTY_ID) String propertyId) throws RGuestException;
+
+    @POST
+    @Path(TAX_SUMMARY_CONFIG)
+    void postTaxSummaryConfigSettings(@PathParam(TENANT_ID) String tenantId, @PathParam(PROPERTY_ID) String propertyId,
+          TaxSummaryConfigSettings taxSummaryConfigSettings) throws RGuestException;
+
+    @PUT
+    @Path(TAX_SUMMARY_CONFIG + "/{id}")
+    TaxSummaryConfigSettings updateTaxSummaryConfigSettings(@PathParam(TENANT_ID) String tenantId,
+          @PathParam(PROPERTY_ID) String propertyId, @PathParam(ID) String id,
+          TaxSummaryConfigSettings taxSummaryConfigSettings) throws RGuestException;
 
 }
---
+++ src/main/java/com/agilysys/pms/account/model/TaxSummaryConfigSettings.java
@@ -0,0 +1,34 @@
+package com.agilysys.pms.account.model;
+
+import java.util.Map;
+
+import com.agilysys.pms.common.model.ObjectBase;
+
+public class TaxSummaryConfigSettings extends ObjectBase {
+    private Map<String, String> fields;
+    private String totalFieldName;
+
+    public TaxSummaryConfigSettings() {
+    }
+
+    public TaxSummaryConfigSettings(Map<String, String> fields, String totalFieldName) {
+        this.fields = fields;
+        this.totalFieldName = totalFieldName;
+    }
+
+    public Map<String, String> getFields() {
+        return fields;
+    }
+
+    public void setFields(Map<String, String> fields) {
+        this.fields = fields;
+    }
+
+    public String getTotalFieldName() {
+        return totalFieldName;
+    }
+
+    public void setTotalFieldName(String totalFieldName) {
+        this.totalFieldName = totalFieldName;
+    }
+}
