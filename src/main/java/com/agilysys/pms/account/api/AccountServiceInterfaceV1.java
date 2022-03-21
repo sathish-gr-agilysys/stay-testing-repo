e44572a0641003537765adb4e4a351a987e07e3b
Sabarivasan <sabarivasan.palanisamy@agilysys.com>
2022-03-21 20:32:13 +0530

    RGSTAY-140087 changes to exclude duplicate updated accounts

--- src/main/java/com/agilysys/pms/account/api/AccountServiceInterfaceV1.java
+++ src/main/java/com/agilysys/pms/account/api/AccountServiceInterfaceV1.java
@@ -314,6 +314,7 @@ public interface AccountServiceInterfaceV1 {
     String SEARCH_BY_UPDATED_DATE = ACCOUNT_TYPE_PATH + "/searchByUpdatedDate";
     String VALIDATE_FOR_REFERENCE_NUMBER = "/validateForReferenceNumber";
     String CANCELLATION = "/cancellation";
+    String RESERVATION_IDS_TO_EXCLUDE = "reservationIdsToExclude";
 
     @GET
     @PreAuthorize("hasPermission('Required', 'ReadAccounts')")
@@ -327,7 +328,8 @@ public interface AccountServiceInterfaceV1 {
     List<AccountSummary> getAccountsByUpdatedTimeRange(@PathParam(TENANT_ID) String tenantId, @PathParam(PROPERTY_ID) String propertyId,
           @PathParam("accountType") String accountTypes,
           @QueryParam(START_DATE_TIME) String startDateTime,
-          @QueryParam(END_DATE_TIME) String endDateTime) throws RGuestException;
+          @QueryParam(END_DATE_TIME) String endDateTime,
+          @QueryParam(RESERVATION_IDS_TO_EXCLUDE) Set<String> reservationIdsToExclude) throws RGuestException;
 
     @Deprecated
     @GET
