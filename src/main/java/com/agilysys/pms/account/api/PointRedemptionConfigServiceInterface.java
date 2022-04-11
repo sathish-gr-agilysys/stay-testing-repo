6e7ffd0f2c4cfb150b1656db58ceca968704fe0b
Sivaprakasam, Sharmila <Sharmila.Sivaprakasam@agilysys.com>
2022-04-11 10:41:00 +0530

    RGSTAY-132122 added config and properties for penn integration

--- src/main/java/com/agilysys/pms/account/api/PointRedemptionConfigServiceInterface.java
+++ src/main/java/com/agilysys/pms/account/api/PointRedemptionConfigServiceInterface.java
@@ -8,6 +8,7 @@ import static com.agilysys.pms.account.api.PointRedemptionConfigServiceInterface
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -17,9 +18,6 @@ import javax.ws.rs.core.MediaType;
 import com.agilysys.platform.common.rguest.exception.RGuestException;
 import com.agilysys.pms.account.model.PointRedemption;
 
-/**
- * GET method for HtngCategory
- */
 @Path(BASE)
 @Produces(MediaType.APPLICATION_JSON)
 public interface PointRedemptionConfigServiceInterface {
@@ -28,17 +26,22 @@ public interface PointRedemptionConfigServiceInterface {
     String PROPERTY_ID = "propertyId";
     String TENANT_TEMPLATE = "{" + TENANT_ID + "}";
     String PROPERTY_TEMPLATE = "{" + PROPERTY_ID + "}";
-    String BASE = "/tenants/" + TENANT_TEMPLATE + "/properties/" + PROPERTY_TEMPLATE +"/config";
+    String BASE = "/tenants/" + TENANT_TEMPLATE + "/properties/" + PROPERTY_TEMPLATE + "/config";
     String POINT_REDEMPTION_FLOW_CONFIG = "/pointRedemptionConfig";
 
     @GET
     @Path(POINT_REDEMPTION_FLOW_CONFIG)
-    List<PointRedemption> getPointRedemption(@PathParam(TENANT_ID) String tenantId,
+    List<PointRedemption> getPointRedemptionConfig(@PathParam(TENANT_ID) String tenantId,
           @PathParam(PROPERTY_ID) String propertyId) throws RGuestException;
 
+    @POST
+    @Path(POINT_REDEMPTION_FLOW_CONFIG)
+    PointRedemption addPointRedemptionConfig(@PathParam(TENANT_ID) String tenantId,
+          @PathParam(PROPERTY_ID) String propertyId, PointRedemption pointRedemption) throws RGuestException;
+
     @PUT
-    @Path(POINT_REDEMPTION_FLOW_CONFIG+"/{id}")
-    PointRedemption updatePointRedemption(@PathParam(TENANT_ID) String tenantId,
+    @Path(POINT_REDEMPTION_FLOW_CONFIG + "/{id}")
+    PointRedemption updatePointRedemptionConfig(@PathParam(TENANT_ID) String tenantId,
           @PathParam(PROPERTY_ID) String propertyId, @PathParam(ID) String id, PointRedemption pointRedemption)
           throws RGuestException;
 
