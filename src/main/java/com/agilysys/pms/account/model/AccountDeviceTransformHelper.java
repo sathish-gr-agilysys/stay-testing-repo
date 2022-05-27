a0065a6fdf55224bd55b5997514b1fa06bb51f5e
Donk, Cale <Cale.Donk@agilysys.com>
2022-05-27 15:52:39 -0700

    RGSTAY-143403: Added common transform methods.

---
+++ src/main/java/com/agilysys/pms/account/model/AccountDeviceTransformHelper.java
@@ -0,0 +1,28 @@
+/*
+ * (C) 2022 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
+ */
+package com.agilysys.pms.account.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public final class AccountDeviceTransformHelper {
+    public static BigDecimal toChargeAmount(List<EstimatedChargesView> estimatedChargesViews) {
+        if (estimatedChargesViews == null) {
+            return null;
+        }
+
+        BigDecimal chargeAmount = BigDecimal.ZERO;
+        for (EstimatedChargesView estimatedChargesView : estimatedChargesViews) {
+            List<RecurringChargeView> recurringCharges = estimatedChargesView.getRecurringCharges();
+            if (recurringCharges != null) {
+                for (RecurringChargeView recurringCharge : recurringCharges) {
+                    BigDecimal normalizedAmount =
+                          recurringCharge.getAmount().multiply(new BigDecimal(recurringCharge.getQuantity()));
+                    chargeAmount = chargeAmount.add(normalizedAmount);
+                }
+            }
+        }
+        return chargeAmount;
+    }
+}
