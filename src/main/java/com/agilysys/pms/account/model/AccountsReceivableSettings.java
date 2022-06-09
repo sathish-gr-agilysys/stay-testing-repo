425b1d398dea6e3391cca76bfd3d4bc7c6d0abe0
PrabhuH <Harish.Prabhu@agilysys.com>
2022-06-09 19:13:54 +0530

    RGSTAY-151202 Added BugFix for RGSTAY-128575, changed showSelectedThirdPartyConfirmation variable to wrapper class from primitive data type

--- src/main/java/com/agilysys/pms/account/model/AccountsReceivableSettings.java
+++ src/main/java/com/agilysys/pms/account/model/AccountsReceivableSettings.java
@@ -33,7 +33,7 @@ public class AccountsReceivableSettings {
 
     private boolean requiredReferenceNumber;
 
-    private boolean showSelectedThirdPartyConfirmation;
+    private Boolean showSelectedThirdPartyConfirmation;
     private Set<ThirdPartyConfirmation> selectedThirdPartyConfirmations;
 
     public BigDecimal getCreditLimit() {
@@ -80,11 +80,11 @@ public class AccountsReceivableSettings {
         return requiredReferenceNumber;
     }
 
-    public void setRequiredReferenceNumber(boolean requiredReferenceNumber) {
+    public void setRequiredReferenceNumber(Boolean requiredReferenceNumber) {
         this.requiredReferenceNumber = requiredReferenceNumber;
     }
 
-    public boolean isShowSelectedThirdPartyConfirmation() {
+    public Boolean isShowSelectedThirdPartyConfirmation() {
         return showSelectedThirdPartyConfirmation;
     }
 
