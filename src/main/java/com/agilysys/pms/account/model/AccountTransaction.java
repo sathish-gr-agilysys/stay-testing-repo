30ad8c1c54f81ab15068e21aa14df06647883518
Sridharan,AravindhaShiva <AravindhaShiva.Sridharan@agilysys.com>
2022-10-19 11:19:52 +0530

    RGSTAY-166047 Refactor group reservation flow

--- src/main/java/com/agilysys/pms/account/model/AccountTransaction.java
+++ src/main/java/com/agilysys/pms/account/model/AccountTransaction.java
@@ -6,23 +6,13 @@ package com.agilysys.pms.account.model;
 import java.math.BigDecimal;
 
 public class AccountTransaction {
-    String accountId;
     BigDecimal transactionAmount;
 
     public AccountTransaction() {
     }
 
-    public AccountTransaction(String accountId, BigDecimal transactionAmount) {
-        this.accountId = accountId;
+    public AccountTransaction(BigDecimal transactionAmount) {
         this.transactionAmount = transactionAmount;
-    }
-
-    public String getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(String accountId) {
-        this.accountId = accountId;
     }
 
     public BigDecimal getTransactionAmount() {
